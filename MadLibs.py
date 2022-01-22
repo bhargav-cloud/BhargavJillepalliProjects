71a447b5abf6b99d82362a3edb75cae9b109eea8
bhargav-cloud <97711682+bhargav-cloud@users.noreply.github.com>
2022-01-21 22:57:41 -0500

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,38 @@
+
+def madlib():
+    plural_noun_1 = input("Plural Noun: ")
+    occupation = input("Occupation: ")
+    location = input("Location: ")
+    number = input("Number: ")
+    adjective_1 = input("Adjective: ")
+    verb_end_ing1 = input("Verb ending with ing: ")
+    plural_noun_2 = input("Plural Noun: ")
+    location_2 = input("Location: ")
+    adjective_2 = input("Adjective: ")
+    plural_noun_3 = input("Plural Noun: ")
+    verb_end_ing2 = input("Verb ending with ing: ")
+    plural_noun_4 = input("Plural Noun: ")
+    adjective_3 = input("Adjective: ")
+    noun = input("Noun: ")
+    part_of_body = input("Part of Body: ")
+    verb = input("Verb: ")
+    adjective_4 = input("Adjective: ")
+    part_of_body2 = input("Part of Body: ")
+
+
+
+    madlibs = f"Hello, my comrade {plural_noun_1} in 2020. It's me, George Washington, \
+ the first {occupation}. I am writing from (the) {location}, where I have been secretly living \
+ for the past {number} years. I am concerned by the {adjective_1} state of affairs in America these days. \
+It seems that your politicians are more concerned with {verb_end_ing1} one another than with listening to the \
+{plural_noun_2} of the people. When we delcared our independence from (the) {location_2}, we set forth on a/an \
+{adjective_2} path guided by the voices of the everyday {plural_noun_3}.If we're going to keep {verb_end_ing2}, then we need to learn \
+how to respect all {plural_noun_4}. Don't get me wrong; we had {adjective_3} problems in my day too. Benjamin Franklin once called me a/an \
+{noun} and kicked me in the {part_of_body}. But at the end of the day, we were able to {verb} in harmony. Let us find that {adjective_4} once again \
+or else I'm taking {part_of_body2} off the quarters!!!!"
+
+
+
+
+
+    print(madlibs)
